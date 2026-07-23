c78866b81d23ebba2802f77efac9785f28666a37
Sebastian Pawlak <sebastian.pawlak@wpengine.com>
2026-07-23 17:00:11 +0200

    Revert "try to catch and log cookie"
    This reverts commit 3927d70a7f21e4d69eeea3e75c1efedcc6cbe76a.

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,44 @@
 import { env } from 'cloudflare:workers'
 import { type ElementHandlers, type HtmlRewriterSession, MOSProxyBuilder } from '@monetizationos/proxy'
-import { parse } from "cookie";
+
+const proxy = new MOSProxyBuilder()
+    .withConfig({
+        originUrl: env.ORIGIN_URL || 'https://example.local',
+        surfaceSlug: env.SURFACE_SLUG ?? '',
+        mosHost: env.MONETIZATION_OS_HOST || 'https://api.monetizationos.com',
+        mosSecretKey: env.MONETIZATION_OS_SECRET_KEY ?? '',
+        mosEndpointsPrefix: env.MONETIZATION_OS_ENDPOINTS_PREFIX || '/mos-endpoints/',
+        anonymousSessionCookieName: env.ANONYMOUS_SESSION_COOKIE_NAME,
+        authenticatedUserJwtCookieName: env.AUTHENTICATED_USER_JWT_COOKIE_NAME,
+        injectScriptUrl: env.INJECT_SCRIPT_URL || undefined,
+        surfaceDecisionsIgnorePaths: env.SURFACE_DECISIONS_IGNORE_PATHS,
+        surfaceDecisionsCookies: env.SURFACE_DECISIONS_COOKIES,
+        originRequestHeaders: env.ORIGIN_REQUEST_HEADERS ?? {},
+    })
+    .withOriginFetcher(fetch)
+    .withApiFetcher(fetch)
+    .withHtmlRewriter({
+        capabilities: {
+            onEndTag: true,
+            nthChild: true,
+        },
+        create() {
+            return new CloudflareHtmlRewriterSession()
+        },
+    })
+    .withClientMetadata({
+        build(request) {
+            return {
+                cloudflare: {
+                    cf: request.cf,
+                },
+            }
+        },
+    })
+    .build()
 
 export default {
     async fetch(request): Promise<Response> {
-        const COOKIE_NAME = "wpe_media_paywall";
-        const cookie = parse(request.headers.get("Cookie") || "");
-        const paywallCookie = cookie[COOKIE_NAME];
-        console.log("Paywall cookie:", paywallCookie);
-
-        const proxy = new MOSProxyBuilder()
-            .withConfig({
-                originUrl: env.ORIGIN_URL || 'https://example.local',
-                surfaceSlug: env.SURFACE_SLUG ?? '',
-                mosHost: env.MONETIZATION_OS_HOST || 'https://api.monetizationos.com',
-                mosSecretKey: env.MONETIZATION_OS_SECRET_KEY ?? '',
-                mosEndpointsPrefix: env.MONETIZATION_OS_ENDPOINTS_PREFIX || '/mos-endpoints/',
-                anonymousSessionCookieName: env.ANONYMOUS_SESSION_COOKIE_NAME,
-                authenticatedUserJwtCookieName: env.AUTHENTICATED_USER_JWT_COOKIE_NAME,
-                injectScriptUrl: env.INJECT_SCRIPT_URL || undefined,
-                surfaceDecisionsIgnorePaths: env.SURFACE_DECISIONS_IGNORE_PATHS,
-                surfaceDecisionsCookies: env.SURFACE_DECISIONS_COOKIES,
-                originRequestHeaders: env.ORIGIN_REQUEST_HEADERS ?? {},
-            })
-            .withOriginFetcher(fetch)
-            .withApiFetcher(fetch)
-            .withHtmlRewriter({
-                capabilities: {
-                    onEndTag: true,
-                    nthChild: true,
-                },
-                create() {
-                    return new CloudflareHtmlRewriterSession()
-                },
-            })
-            .withClientMetadata({
-                build(request) {
-                    return {
-                        cloudflare: {
-                            cf: request.cf,
-                        },
-                    }
-                },
-            })
-            .build()
-
         return proxy.handle(request)
     },
 } satisfies ExportedHandler<Env>
