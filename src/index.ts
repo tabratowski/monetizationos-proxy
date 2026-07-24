e6e48a3d3d2cc44d0b38067af2594b2023923dbb
Sebastian Pawlak <sebastian.pawlak@wpengine.com>
2026-07-24 15:48:46 +0200

    test proxy

--- src/index.ts
+++ src/index.ts
@@ -2,42 +2,6 @@ import { env } from 'cloudflare:workers'
 import { type ElementHandlers, type HtmlRewriterSession, MOSProxyBuilder } from '@monetizationos/proxy'
 import { parse } from "cookie";
 
-const proxy = new MOSProxyBuilder()
-    .withConfig({
-        originUrl: env.ORIGIN_URL || 'https://example.local',
-        surfaceSlug: env.SURFACE_SLUG ?? '',
-        mosHost: env.MONETIZATION_OS_HOST || 'https://api.monetizationos.com',
-        mosSecretKey: env.MONETIZATION_OS_SECRET_KEY ?? '',
-        mosEndpointsPrefix: env.MONETIZATION_OS_ENDPOINTS_PREFIX || '/mos-endpoints/',
-        anonymousSessionCookieName: env.ANONYMOUS_SESSION_COOKIE_NAME,
-        authenticatedUserJwtCookieName: env.AUTHENTICATED_USER_JWT_COOKIE_NAME,
-        injectScriptUrl: env.INJECT_SCRIPT_URL || undefined,
-        surfaceDecisionsIgnorePaths: env.SURFACE_DECISIONS_IGNORE_PATHS,
-        surfaceDecisionsCookies: env.SURFACE_DECISIONS_COOKIES,
-        originRequestHeaders: env.ORIGIN_REQUEST_HEADERS ?? {},
-    })
-    .withOriginFetcher(fetch)
-    .withApiFetcher(fetch)
-    .withHtmlRewriter({
-        capabilities: {
-            onEndTag: true,
-            nthChild: true,
-        },
-        create() {
-            return new CloudflareHtmlRewriterSession()
-        },
-    })
-    .withClientMetadata({
-        build(request) {
-            return {
-                cloudflare: {
-                    cf: request.cf,
-                },
-            }
-        },
-    })
-    .build()
-
 export default {
     async fetch(request): Promise<Response> {
         console.log("request: ", request.url)
@@ -56,6 +20,43 @@ export default {
         console.log("JSON paywall cookie's mosSecretKey:", jsonPaywallCookie.mosSecretKey);
         console.log("JSON paywall cookie's originUrl:", jsonPaywallCookie.originUrl);
         console.log("JSON paywall cookie's surfaceSlug:", jsonPaywallCookie.surfaceSlug);
+
+        const proxy = new MOSProxyBuilder()
+            .withConfig({
+                originUrl: jsonPaywallCookie.originUrl || 'https://example.local',
+                surfaceSlug: jsonPaywallCookie.surfaceSlug ?? '',
+                mosHost: env.MONETIZATION_OS_HOST || 'https://api.monetizationos.com',
+                mosSecretKey: jsonPaywallCookie.mosSecretKey ?? '',
+                mosEndpointsPrefix: env.MONETIZATION_OS_ENDPOINTS_PREFIX || '/mos-endpoints/',
+                anonymousSessionCookieName: env.ANONYMOUS_SESSION_COOKIE_NAME,
+                authenticatedUserJwtCookieName: env.AUTHENTICATED_USER_JWT_COOKIE_NAME,
+                injectScriptUrl: env.INJECT_SCRIPT_URL || undefined,
+                surfaceDecisionsIgnorePaths: env.SURFACE_DECISIONS_IGNORE_PATHS,
+                surfaceDecisionsCookies: env.SURFACE_DECISIONS_COOKIES,
+                originRequestHeaders: env.ORIGIN_REQUEST_HEADERS ?? {},
+            })
+            .withOriginFetcher(fetch)
+            .withApiFetcher(fetch)
+            .withHtmlRewriter({
+                capabilities: {
+                    onEndTag: true,
+                    nthChild: true,
+                },
+                create() {
+                    return new CloudflareHtmlRewriterSession()
+                },
+            })
+            .withClientMetadata({
+                build(request) {
+                    return {
+                        cloudflare: {
+                            cf: request.cf,
+                        },
+                    }
+                },
+            })
+            .build()
+
         return await proxy.handle(request);
     },
 } satisfies ExportedHandler<Env>
