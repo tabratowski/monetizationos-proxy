3c037c21505e79567b238df5af5ad1ad3cc32c52
Sebastian Pawlak <sebastian.pawlak@wpengine.com>
2026-08-07 17:18:46 +0200

    reset

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,46 @@
 import { env } from 'cloudflare:workers'
 import { type ElementHandlers, type HtmlRewriterSession, MOSProxyBuilder } from '@monetizationos/proxy'
-import { parse } from "cookie";
+
+const proxy = new MOSProxyBuilder()
+    .withConfig({
+        originUrl: env.ORIGIN_URL || 'https://example.local',
+        surfaceSlug: env.SURFACE_SLUG ?? '',
+        mosHost: env.MONETIZATION_OS_HOST || 'https://api.monetizationos.com',
+        mosSecretKey: env.MONETIZATION_OS_SECRET_KEY ?? '',
+        mosEndpointsPrefix: env.MONETIZATION_OS_ENDPOINTS_PREFIX || '/mos-endpoints/',
+        anonymousSessionCookieName: env.ANONYMOUS_SESSION_COOKIE_NAME,
+        authenticatedUserJwtCookieName: env.AUTHENTICATED_USER_JWT_COOKIE_NAME,
+        injectScriptUrl: env.INJECT_SCRIPT_URL || undefined,
+        surfaceDecisionsIgnorePaths: env.SURFACE_DECISIONS_IGNORE_PATHS,
+        surfaceDecisionsCookies: env.SURFACE_DECISIONS_COOKIES,
+        originRequestHeaders: env.ORIGIN_REQUEST_HEADERS ?? {},
+    })
+    .withOriginFetcher(fetch)
+    .withApiFetcher(fetch)
+    .withHtmlRewriter({
+        capabilities: {
+            onEndTag: true,
+            nthChild: true,
+        },
+        create() {
+            return new CloudflareHtmlRewriterSession()
+        },
+    })
+    .withClientMetadata({
+        build(request) {
+            return {
+                cloudflare: {
+                    cf: request.cf,
+                },
+            }
+        },
+    })
+    .withClientIP((request) => request.headers.get('CF-Connecting-IP') ?? undefined)
+    .build()
 
 export default {
     async fetch(request): Promise<Response> {
-        const cookies = parse(request.headers.get("X-Forwarded-Cookies") || "");
-        const paywallCookie = cookies["wpe_media_paywall"];
-        if (!paywallCookie) {
-            return new Response("Missing paywall cookie", { status: 400 });
-        }
-
-        const decodedPaywallCookie = atob(decodeURIComponent(paywallCookie));
-        const jsonPaywallCookie = JSON.parse(decodedPaywallCookie);
-        console.log("JSON paywall cookie's mosSecretKey:", jsonPaywallCookie.mosSecretKey);
-        console.log("JSON paywall cookie's originUrl:", jsonPaywallCookie.originUrl);
-        console.log("JSON paywall cookie's surfaceSlug:", jsonPaywallCookie.surfaceSlug);
-
-        // Set X-Bypass-Snippet to avoid circular calls
-        // request.headers.set('X-Bypass-Snippet', 'true'); -> TypeError: Can't modify immutable headers.
-        const newHeaders = new Headers(request.headers);
-        newHeaders.set('X-Bypass-Snippet', 'true');
-        const newRequest = new Request(request, { headers: newHeaders });
-
-        const proxy = new MOSProxyBuilder()
-            .withConfig({
-                originUrl: jsonPaywallCookie.originUrl,
-                surfaceSlug: jsonPaywallCookie.surfaceSlug,
-                mosHost: env.MONETIZATION_OS_HOST || 'https://api.monetizationos.com',
-                mosSecretKey: jsonPaywallCookie.mosSecretKey,
-                mosEndpointsPrefix: env.MONETIZATION_OS_ENDPOINTS_PREFIX || '/mos-endpoints/',
-                anonymousSessionCookieName: env.ANONYMOUS_SESSION_COOKIE_NAME,
-                authenticatedUserJwtCookieName: env.AUTHENTICATED_USER_JWT_COOKIE_NAME,
-                injectScriptUrl: env.INJECT_SCRIPT_URL || undefined,
-                surfaceDecisionsIgnorePaths: env.SURFACE_DECISIONS_IGNORE_PATHS,
-                surfaceDecisionsCookies: env.SURFACE_DECISIONS_COOKIES,
-                originRequestHeaders: env.ORIGIN_REQUEST_HEADERS ?? {},
-            })
-            .withOriginFetcher(fetch)
-            .withApiFetcher(fetch)
-            .withHtmlRewriter({
-                capabilities: {
-                    onEndTag: true,
-                    nthChild: true,
-                },
-                create() {
-                    return new CloudflareHtmlRewriterSession()
-                },
-            })
-            .withClientMetadata({
-                build(request) {
-                    return {
-                        cloudflare: {
-                            cf: request.cf,
-                        },
-                    }
-                },
-            })
-            .build()
-
-        return await proxy.handle(newRequest);
+        return proxy.handle(request)
     },
 } satisfies ExportedHandler<Env>
 
